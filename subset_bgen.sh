cbcb64b452a5e598962783a0581fa43c9db46217
martarii <30492121+martarii@users.noreply.github.com>
2022-01-19 15:57:15 +0100

    Create subset_bgen.sh

---
+++ subset_bgen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+qctool=/fullpath/qctool
+
+for i in {1:22} X
+do
+  ${qctool} -g ${i}.bgen -s ${i}.sample -incl-samples trace_element_fid.txt -og ${i}_subset.bgen -os ${i}_subset.sample
+done
