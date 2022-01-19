6ece4dcd4ff067bb928e075d277c265f16c37a8d
martarii <30492121+martarii@users.noreply.github.com>
2022-01-19 15:50:58 +0100

    Create create_snplists_for_genotyped_pruned.sh

---
+++ create_snplists_for_genotyped_pruned.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in {1..22} {X..X}
+do
+  bcftools query -f "%ID\t%INFO/INFO\t%INFO/AC\n" ${i}.vcf.gz | awk '$2>0.99 && $3>1963 && $3<194257 {print $1}' > SNPlist${i}.txt
+done
+
