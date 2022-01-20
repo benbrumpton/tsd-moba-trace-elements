63356ddd69a421dc95657ffa214f62651513a8e0
martarii <30492121+martarii@users.noreply.github.com>
2022-01-20 15:27:00 +0100

    Update and rename create_snplists_for_genotyped_pruned.sh to create_genotyped_pruned.sh

---
+++ create_genotyped_pruned.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {1..22} X
+do
+  bcftools query -f "%ID\t%FORMAT/ID\t%INFO/INFO\t%INFO/AC\n" ${i}.vcf.gz | awk '$2=="TYPED" && $3>0.99 && $4>1963 && $4<194257 {print $1}' > SNPlist${i}.txt
+done
+wait
+
+for i in {1..22} X
+do plink2 --bfile ${i} --keep trace_element_fid_iid.txt --extract SNPlist${i}.txt --indep-pairwise 50 5 0.8 --make-bed --out chr${i}_pruned
+done
