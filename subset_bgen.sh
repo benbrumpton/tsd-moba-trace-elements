fc5311a3697a43167ab43116e9ec78fcbe02c3df
martarii <30492121+martarii@users.noreply.github.com>
2022-01-25 10:37:34 +0100

    Update subset_bgen.sh

--- subset_bgen.sh
+++ subset_bgen.sh
@@ -4,5 +4,5 @@ qctool=/fullpath/qctool
 
 for i in {1:22} X
 do
-  ${qctool} -g ${i}.bgen -s ${i}.sample -incl-samples trace_element_fid.txt -og ${i}_subset.bgen -os ${i}_subset.sample -log ${i}_subset.log
+  ${qctool} -g ${i}.bgen -s ${i}.sample -incl-samples trace_element_fid.txt -bgen-bits 8 -og ${i}_subset.bgen -os ${i}_subset.sample -log ${i}_subset.log
 done
