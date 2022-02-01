2b4c8b4098bce0bef6001968838c97da3b46a657
martarii <30492121+martarii@users.noreply.github.com>
2022-02-01 16:30:31 +0100

    Update make_pheno.R
    Create phenofile for all trace elements + summarise distributions

--- make_pheno.R
+++ make_pheno.R
@@ -46,16 +46,62 @@ tmp2 <- merge(trace_elements,tmp1,by="M_ID_2944",all=F)
 tmp2$MATID <- 0
 tmp2$PATID <- 0
 
-#Create one phenofile per trace element and inverse rank normalize residuals
 
-Mn_pheno <- tmp2[,c("FID","IID","MATID","PATID","Mn_Mangan","AgeSample","PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10")]
-Mn_pheno <- na.omit(Mn_pheno)
-regr <- lm(Mn_Mangan~AgeSample,Mn_pheno)
-residuals <- resid(regr)
-Mn_pheno$Mn.rn <- RankNorm(residuals)
+#Rename variables
+tmp2$Mn.Manganese <- tmp2$Mn_Mangan
+tmp2$Co.Cobalt <- tmp2$Co_Kobalt
+tmp2$Cu.Copper <- tmp2$Cu_Kobber
+tmp2$Zn.Zinc <- tmp2$Zn_Sink
+tmp2$As.Arsenic <- tmp2$As_Arsen
+tmp2$Se.Selenium <- tmp2$Se_Selen
+tmp2$Mo.Molybdenum <- tmp2$Mo_Molybdeno
+tmp2$Cd.Cadmium <- tmp2$Cd_Kadmium
+tmp2$Tl.Thallium <- tmp2$Tl_Tallium
+tmp2$Pb.Lead <- tmp2$Pb_Bly
+tmp2$Hg.Mercury <- tmp2$Hg_total
 
-write.table(Mn_pheno[,c(1:4,6:17)],"moba_Mn_pheno_120122.txt",quote=F,sep="\t",col.names=T,row.names=F)
+element_names <- c("Mn.Manganese","Co.Cobalt","Cu.Copper","Zn.Zinc","As.Arsenic","Se.Selenium","Mo.Molybdenum","Cd.Cadmium","Tl.Thallium","Pb.Lead","Hg.Mercury")
 
-#Repeat for the other trace elements
+#Create one phenofile for each trace element (rank transformed to normality)
+library(RNOmni)
+for (i in 1:length(element_names)){
+  trace_element <- element_names[i]
+  fields <- c("FID","IID","MATID","PATID","Mn_Mangan","AgeSample","PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10",trace_element)
+  pheno <- tmp2[,fields]
+  pheno <- na.omit(pheno)
+  
+  #Rank transform to normality (equivalent of rntransform)
+  regr <- lm(pheno[,trace_element]~pheno$AgeSample)
+  residuals <- resid(regr)
+  
+  twoletter <- substr(trace_element,1,2)
+  outname <- paste0(twoletter,".rn")
+  
+  pheno[,outname] <- RankNorm(residuals)
+  write.table(pheno[,c(1:15,17)],paste0("moba_",twoletter,"_pheno_120122.txt"),quote=F,sep="\t",col.names=T,row.names=F)
+}
+
+#Create summary table with distributions of trace elements before transformations:
+library(tidyr)
+df.sum <- tmp2 %>% select(element_names) %>% summarize_each(funs(min=min(.,na.rm=T),q25=quantile(.,0.25,na.rm=T),median=median(.,na.rm=T),q75=quantile(.,0.75,na.rm=T),max=max(.,na.rm=T),mean=mean(.,na.rm=T),sd=sd(.,na.rm=T),n=length(which(!is.na(.))) ))
+dim(df.sum)
+
+df.stats.tidy <- df.sum %>% gather(stat,val) %>%
+    separate(stat,into=c("element","stat"),sep="_") %>%
+    spread(stat,val) %>%
+    select(element,min,q25,median,q75,max,mean,sd,n)
+
+df.stats.tidy$element <- substring(df.stats.tidy$element,4)
+
+write.table(df.stats.tidy,"MOBA_trace_element_distributions.txt",quote=F,sep="\t",col.names=T,row.names=F)
+
+
+#Mn_pheno <- tmp2[,c("FID","IID","MATID","PATID","Mn_Mangan","AgeSample","PC1","PC2","PC3","PC4","PC5","PC6","PC7","PC8","PC9","PC10")]
+#Mn_pheno <- na.omit(Mn_pheno)
+#regr <- lm(Mn_Mangan~AgeSample,Mn_pheno)
+#residuals <- resid(regr)
+#Mn_pheno$Mn.rn <- RankNorm(residuals)
+#
+#write.table(Mn_pheno[,c(1:4,6:17)],"moba_Mn_pheno_120122.txt",quote=F,sep="\t",col.names=T,row.names=F)
 
 
