b6a48e5ddb379aebf048a62ea415093fd752a654
martarii <30492121+martarii@users.noreply.github.com>
2022-03-15 17:21:59 +0100

    Create combine_plink_res.sh

---
+++ combine_plink_res.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Combining results from running plink separately on each chromosome for As and Mo
+awk '
+    FNR==1 && NR!=1 {while (/^<header>/) getline;}
+    1 {print}
+' chr*.As.rn.glm.linear > all_chr.As.rn.glm.linear.txt
+
+awk '
+    FNR==1 && NR!=1 {while (/^<header>/) getline;}
+    1 {print}
+' chr*.Mo.rn.glm.linear > all_chr.Mo.rn.glm.linear.txt
